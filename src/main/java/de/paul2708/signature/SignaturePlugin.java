0a224e24338f5be4586ca920827ae200dc89b73b
Paul2708 <paul.hoger@gmx.net>
2020-07-26 15:44:13 +0200

    Add correct permissions and messages

--- src/main/java/de/paul2708/signature/SignaturePlugin.java
+++ src/main/java/de/paul2708/signature/SignaturePlugin.java
@@ -1,5 +1,6 @@
 package de.paul2708.signature;
 
+import de.paul2708.signature.command.AbstractCommand;
 import de.paul2708.signature.command.SignCommand;
 import de.paul2708.signature.command.UnsignCommand;
 import de.paul2708.signature.configuration.PluginConfiguration;
@@ -14,9 +15,16 @@ public final class SignaturePlugin extends JavaPlugin {
 
         ItemSigner signer = new ItemSigner(configuration.getFormat());
 
-        getCommand("sign").setExecutor(new SignCommand("sign", "no_permission",
-                configuration, signer));
-        getCommand("unsign").setExecutor(new UnsignCommand("unsign", "no_permission",
-                configuration, signer));
+        AbstractCommand signCommand = new SignCommand(
+                configuration.getPermission("sign"),
+                configuration.getMessage("no_permission"),
+                configuration, signer);
+        AbstractCommand unsignCommand = new UnsignCommand(
+                configuration.getPermission("unsign"),
+                configuration.getMessage("no_permission"),
+                configuration, signer);
+
+        getCommand("sign").setExecutor(signCommand);
+        getCommand("unsign").setExecutor(unsignCommand);
     }
 }
