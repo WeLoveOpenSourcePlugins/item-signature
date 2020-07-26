bc2a01b018756e76690fff2777185cee4d5ca4da
Paul2708 <paul.hoger@gmx.net>
2020-07-26 14:52:51 +0200

    Add abstract command interface and the sign command

--- src/main/java/de/paul2708/signature/SignaturePlugin.java
+++ src/main/java/de/paul2708/signature/SignaturePlugin.java
@@ -1,7 +1,10 @@
 package de.paul2708.signature;
 
+import de.paul2708.signature.command.SignCommand;
 import de.paul2708.signature.configuration.PluginConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class SignaturePlugin extends JavaPlugin {
 
@@ -9,5 +12,8 @@ public final class SignaturePlugin extends JavaPlugin {
     public void onEnable() {
         PluginConfiguration configuration = new PluginConfiguration(this);
         configuration.load();
+
+        getCommand("sign").setExecutor(new SignCommand("sign", "no_permission",
+                configuration, new ItemSigner(List.of("test1", "test2"))));
     }
 }
---
+++ src/main/java/de/paul2708/signature/command/SignCommand.java
@@ -0,0 +1,33 @@
+package de.paul2708.signature.command;
+
+import de.paul2708.signature.ItemSigner;
+import de.paul2708.signature.configuration.PluginConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class SignCommand extends AbstractCommand {
+
+    private final PluginConfiguration configuration;
+    private final ItemSigner signer;
+
+    public SignCommand(String permission, String noPermissionMessage, PluginConfiguration configuration,
+                       ItemSigner signer) {
+        super(permission, noPermissionMessage);
+
+        this.configuration = configuration;
+        this.signer = signer;
+    }
+
+    @Override
+    protected void run(Player player, String[] args) {
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        if (!signer.isValid(item)) {
+            player.sendMessage(configuration.getMessage("no_item_in_hand"));
+            return;
+        }
+
+        signer.sign(item);
+        player.sendMessage(configuration.getMessage("successfully_signed"));
+    }
+}
