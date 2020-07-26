c950022732c0d7ad38699d6a0fce4d6f0a8c1946
Paul2708 <paul.hoger@gmx.net>
2020-07-26 12:21:32 +0200

    Add main plugin class and its plugin.yml

---
+++ src/main/java/de/paul2708/signature/SignaturePlugin.java
@@ -0,0 +1,11 @@
+package de.paul2708.signature;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SignaturePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Running.");
+    }
+}
